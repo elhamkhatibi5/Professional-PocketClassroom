b670986ae868d8c80d522bb54253f79a2ef7e7ca
elhamkhatibi5 <elhamkhatibi5@gmail.com>
2025-10-18 08:32:37 +0430

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,15 @@
-// utils.js placeholder
+// ====== Utils ======
+
+// زمان انسانی
+function timeAgo(date) {
+  const diff = (new Date() - new Date(date)) / 1000;
+  if(diff < 60) return `${Math.floor(diff)} sec ago`;
+  if(diff < 3600) return `${Math.floor(diff/60)} min ago`;
+  if(diff < 86400) return `${Math.floor(diff/3600)} h ago`;
+  return `${Math.floor(diff/86400)} d ago`;
+}
+
+// Escape HTML
+function escapeHTML(str){
+  return str.replace(/[&<>"']/g, m => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
+}
